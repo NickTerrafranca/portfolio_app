21b525e54af08b3ae3f9f8314e2c08156ab3f879
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-02-17 16:06:00 -0500

    Added options to configure pony. Moved pony logic to model layer

--- models/models.rb
+++ models/models.rb
@@ -1,4 +1,5 @@
 require 'pg'
+require 'pony'
 require 'pry'
 
 def db_connection
@@ -17,4 +18,22 @@ def save_message(name, email, content)
   connection = PG.connect(dbname: 'portfolio_app')
   connection.exec_params(sql, [name, email, content])
   connection.close
+end
+
+def email_message(name, email, content)
+  Pony.options = {
+    :subject => "Portfolio page: Message delivery from #{params[:name]}",
+    :body => "#{params[:content]}",
+    :via => :smtp,
+    :via_options => {
+      :address              => 'smtp.1and1.com',
+      :port                 =>  '587', #'465',
+      :enable_starttls_auto => true,
+      :user_name            => 'noreply@nterrafranca.com', #ENV["USER_EMAIL_ADDRESS"]
+      :password             => "0Mu8-gg2-in8", #ENV["SMTP_PASSWORD"],
+      :authentication       => :login, #:plain, # :plain, :login, :cram_md5, no auth by default
+      :domain               => 'nterrafranca.com' #ENV["DESTINATION_EMAIL_ADDRESS"]
+    }
+  }
+  Pony.mail(:to => 'nterrafranca@icloud.com')
 end
