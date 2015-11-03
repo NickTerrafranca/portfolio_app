4dcc8d215417ed8bf49905d148b7a355ca6a02cf
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-11-03 17:56:57 -0500

    Setting up new mail functionality with sendgrid

--- models/models.rb
+++ models/models.rb
@@ -2,18 +2,20 @@ Dotenv.load
 
 def email_message(name, email, content)
   Pony.options = {
+    :via => :smtp,
     :subject => "Portfolio page: Message delivery from #{name}",
     :body => "From: #{name}, #{email}\n#{content}",
-    :via => :smtp,
     :via_options => {
-      :address              => ENV["SMTP_ADDRESS"],
-      :port                 => ENV["PORT"],
-      :enable_starttls_auto => true,
-      :user_name            => ENV["SENDGRID_USERNAME"],
-      :password             => ENV["SENDGRID_PASSWORD"],
-      :authentication       => :plain,
-      :domain               => 'heroku.com'
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
     }
   }
   Pony.mail(:to => ENV["DESTINATION_EMAIL_ADDRESS"])
 end
+
+
