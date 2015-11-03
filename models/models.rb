bafa401602fd79639f63fd141ffdd15932817cf0
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-11-03 17:21:00 -0500

    Working on new email setup

--- models/models.rb
+++ models/models.rb
@@ -2,17 +2,17 @@ Dotenv.load
 
 def email_message(name, email, content)
   Pony.options = {
-    :subject => "Portfolio page: Message delivery from #{params[:name]}.",
+    :subject => "Portfolio page: Message delivery from #{params[:name]}",
     :body => "From: #{params[:name]}, #{params[:email]}\n#{params[:content]}",
     :via => :smtp,
     :via_options => {
       :address              => ENV["SMTP_ADDRESS"],
       :port                 => ENV["PORT"],
       :enable_starttls_auto => true,
-      :user_name            => ENV['SENDGRID_USERNAME'],
-      :password             => ENV['SENDGRID_PASSWORD'],
+      :user_name            => ENV["SENDGRID_USERNAME"],
+      :password             => ENV["SENDGRID_PASSWORD"],
       :authentication       => :plain,
-      :domain               => ENV["DOMAIN"]
+      :domain               => 'heroku.com'
     }
   }
   Pony.mail(:to => ENV["DESTINATION_EMAIL_ADDRESS"])
